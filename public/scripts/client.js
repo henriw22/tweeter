8f55016737ecfa0fec6e2ca81b3f1c068c2a4011
henriw22 <henriw22@gmail.com>
2021-06-16 23:26:54 +0000

    created dynamic tweets function, form submission using jQuery and fetching tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,9 +5,74 @@
  */
 
 
-$(document).ready(function() {
-  const timeElement = $(".time")
-  const time = timeElement.data("time")
-  const timeAgo = timeago.format(time)
-  timeElement.text(timeAgo)
+$(() => {
+
+  const createTweetElement = (data) => {
+    const name = data.user.name;
+    const avatar = data.user.avatars;
+    const handle = data.user.handle;
+    const content = data.content.text;
+    const time = data.created_at;
+
+    const $tweet = $(`
+      <article class="tweet boxhover">
+        <header>
+          <span class="name"><img src="${avatar}"><span>${name}</span></span>
+          <span class="handle">${handle}</span>
+        </header>
+        <p><b>${content}</b></p>
+        <footer>
+          <span>${time}</span>
+          <!-- <span class=‘time’ data-time=“1623621475652”></span> -->
+          <span class="symbol"><i class="fa fa-flag hover"></i><i class="fa fa-retweet hover" aria-hidden="true"></i><i class="fa fa-heart hover"></i></span>
+        </footer>
+      </article>
+    `);
+
+
+    console.log('tweet:', $tweet);
+    return $tweet;
+  };
+
+
+  const renderTweets = (datas) => {
+    for (let data of datas) {
+      let $tweet = createTweetElement(data);
+      $('#tweets-container').append($tweet);
+    }
+  };
+
+
+  $("form").submit(function (event) {
+    event.preventDefault();
+    const data = $('form').serialize();
+
+    $.ajax({
+      url: "/tweets/",
+      metod: "POST",
+      data: data
+    });
+    console.log('test');
+
+  });
+
+
+
+  const loadTweets = () => {
+    $.ajax('/tweets/', {METHOD: 'GET'})
+    .then((result) => {
+      renderTweets(result);
+    });
+  };
+
+  loadTweets();
+
 });
+
+
+// $(document).ready(function() {
+//   const timeElement = $(".time")
+//   const time = timeElement.data("time")
+//   const timeAgo = timeago.format(time)
+//   timeElement.text(timeAgo)
+// });
